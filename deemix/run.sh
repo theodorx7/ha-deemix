e11b6a52d39d6b90a222b90a44b2d8f243b1c0ca
Fedor <99128059+theodorx7@users.noreply.github.com>
2026-09-12 20:00:42 +0700

    Update run.sh

--- deemix/run.sh
+++ deemix/run.sh
@@ -1,1 +1,8 @@
+#!/usr/bin/with-contenv bashio
 
+set -e
+bashio::log.info "Starting Deemix App..."
+export DEEMIX_SERVER_PORT="$(bashio::config 'server_port')"
+export DEEMIX_HOST="$(bashio::config 'host')"
+bashio::log.info "Server Port: ${DEEMIX_SERVER_PORT}, Host: ${DEEMIX_HOST}"
+exec /init

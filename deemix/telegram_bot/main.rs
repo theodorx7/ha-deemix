e724a662c6ec43dda4bf48adc57dd5537395f0a7
Fedor <99128059+theodorx7@users.noreply.github.com>
2026-09-20 00:49:31 +0700

    Update main.rs

--- deemix/telegram_bot/main.rs
+++ deemix/telegram_bot/main.rs
@@ -67,7 +67,6 @@ impl Config {
                 .unwrap_or_default(),
             users_file: env::var("USERS_FILE")
                 .unwrap_or_else(|_| "/config/telegram_bot/users.json".to_string()),
-                .unwrap_or_else(|_| "/app/users.json".to_string()),
             audd_api_key: env::var("AUDD_API_KEY").unwrap_or_default(),
             openai_api_key: env::var("OPENAI_API_KEY").unwrap_or_default(),
             whisper_url: env::var("WHISPER_URL").unwrap_or_default(),
